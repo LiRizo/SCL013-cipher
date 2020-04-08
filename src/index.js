61c3c5723513de28bc423526f4e48007a19d23ef
“LiRizo” <lirizoc@gmail.com>
2020-04-08 09:47:51 -0400

    html + JS

--- src/index.js
+++ src/index.js
@@ -1,70 +1,165 @@
-/*import cipher from './cipher.js';
+//Vamos a ver que sale XD
 
-console.log(cipher);*/
-
-/* Codigo Cesar
+/*Guia del abecedario
 
 A B C D E F G H I J K  L  M 
 0 1 2 3 4 5 6 7 8 9 10 11 12
 
 N  O  P  Q  R  S  T  U  V  W  X  Y  Z 
 13 14 15 16 17 18 19 20 21 22 23 24 25
+*/
+
+/* Formula ASCII 
+
+(X - 65 + n) % 26 + 65
 
 */
-/*Length En cadenas ( String )
-El valor de la propiedad length en una cadena es igual a su número de caracteres.*/
-//El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
-//El charCodeAt() método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.
 //codigoASCII => codigo0a25 => desloco => giro => codigoASCII
-
-
+/*---->>VEMOS!!
 function rot13(str){
-  var solution = '';
-  for (var i = 0; i < str.length; i++){
-    var asciiNum = str[i].charCodeAt();
-    if (asciiNum >= 65 && asciiNum <= 90){
-      solution += String.fromCharCode(asciiNum + 13);
-    } else if (asciiNum >= 78 && asciiNum <= 90){
-      solution += String.fromCharCode(asciiNum - 13);
-    } 
+    var solution = '';
+    for (var i = 0; i < str.length; i++){
+      var asciiNum = str[i].charCodeAt();
+      if (asciiNum >= 65 && asciiNum <= 90){
+        solution += String.fromCharCode(asciiNum + 13);
+      } else if (asciiNum >= 78 && asciiNum <= 90){
+        solution += String.fromCharCode(asciiNum - 13);
+      } 
+    }
+    return solved; 
   }
-  return solved; 
+*/
+
+function cifrado(message,nfijo){
+  const textoIngresado = texto.value;
+  textoCifrado.value = textoIngresado.split('').map(c=>{
+      let mayus = (c == c.toUpperCase()) 
+  })
 }
 
-rot13('z')
-/
-  /*
-// indice + 33 módulo de 26 = posición Nueva letra
-(0+33)%26 //---> 7 (A = H)
-(4+33)%26 //---> 11 (E = L) 
-((índice - 33) módulo 26) + 26 = Posición antigua Letra
-(0-33)%26 //---> -0 (H = A)
-(11-33)%26 //--> -22 --> -22 + 26 = 4 (L = E)
-(79+65-33)%26+65 //  72 --> inverso
-' ' = 32
-*/
-
-
-
-/*function cifrado()*/
+//Botones, para movernos por nuestra pagina !!
 
 function escogerCifrar() {
 
-	let siguiente = document.getElementById("cifrar1");
-    siguiente.style.display = "block";
 
-    let bienvenida = document.getElementById("bienvenida");
-    bienvenida.style.display = "none";
-	
+let siguiente = document.getElementById("cifrar1");
+  siguiente.style.display = "block";
+
+
+
+  let bienvenida = document.getElementById("bienvenida");
+  bienvenida.style.display = "none";	
 }
-
 function escogerDescifrar() {
+let siguiente = document.getElementById("descifrar1");
+  siguiente.style.display = "block";
 
-	let siguiente = document.getElementById("descifrar1");
-    siguiente.style.display = "block";
-
-    let bienvenida = document.getElementById("bienvenida");
-    bienvenida.style.display = "none";
-	
+  let bienvenida = document.getElementById("bienvenida");
+  bienvenida.style.display = "none";	
 }
 
+//Parte para cifrar mensaje !!
+function cifrarSms(){
+
+
+let cifrarSmsActual = document.getElementById("cifrar1");
+cifrarSmsActual.style.display = "none";
+
+// Esta parte es de los textarea
+let n=0;
+let n=document.getElementById("numero").value;
+
+let escrito = document.getElementById("textoCifrar1").value;
+let texto = "";
+let termino = "";
+let i;
+
+for (i = 0; i < escrito.length; i++) {
+
+if (escrito.charCodeAt(i)>=65 && escrito.charCodeAt(i)<=90){
+  texto= (((escrito.charCodeAt(i)-65)+parseInt(n))%26)+65;
+}
+else if(escrito.charCodeAt(i)===32){
+  texto= 32;
+}
+termino += String.fromCharCode(texto);
+document.getElementById("resultado").innerHTML = termino;
+}
+// termina textarea 
+let resultadoDelSms = document.getElementById("cifrar2");
+resultadoDelSms.style.display = "block";
+}
+
+function cifrarSmsDeVuelta(){
+
+
+let resultadoDelSmsActual = document.getElementById("cifrar2");
+resultadoDelSmsActual.style.display = "none";
+
+let cifrarSms = document.getElementById("cifrar1");
+cifrarSms.style.display = "block";
+}
+
+
+function regresarInicio1(){
+
+
+let resultadoDelSmsActual = document.getElementById("cifrar2");
+resultadoDelSmsActual.style.display = "none";
+
+let inicio = document.getElementById("bienvenida");
+inicio.style.display = "block";
+}
+
+//Parte para descifrar mensaje !!
+
+function descifrarCod(){                       
+
+
+let recibirCodActual = document.getElementById("descifrar1");
+recibirCodActual.style.display = "none";
+
+let inicioSiguiente = document.getElementById("descifrar2");
+inicioSiguiente.style.display = "block";
+}
+
+function regresarInicio2(){
+
+
+let volverInicioActual = document.getElementById("descifrar2");
+volverInicioActual.style.display = "none";
+
+let inicioSiguiente = document.getElementById("bienvenida");
+inicioSiguiente.style.display = "block";
+}
+
+function descifrarlo(){
+
+
+let descifrarAtual = document.getElementById("descifrar2");
+descifrarAtual.style.display = "none";
+
+let descifradoSiguiente = document.getElementById("descifrar3");
+descifradoSiguiente.style.display = "block";
+}
+
+function cifrarSmsV1(){
+
+
+let descifradoAtual = document.getElementById("descifrar3");
+descifradoAtual.style.display = "none";
+
+let cifrarSiguiente = document.getElementById("cifrar1");
+cifrarSiguiente.style.display = "block";
+}
+
+
+function regresarInicio3(){
+
+
+let volverInicioActual = document.getElementById("descifrar3");
+volverInicioActual.style.display = "none";
+
+let inicioSiguiente = document.getElementById("bienvenida");
+inicioSiguiente.style.display = "block";
+}
