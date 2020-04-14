d7d1a8e041bb45b697d00524cf6d33afea0de1dd
“LiRizo” <lirizoc@gmail.com>
2020-04-14 18:23:53 -0400

    carpeta SCL013-cipher

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,34 +1,23 @@
 // Importamos el objeto `cipher`, que contiene los métodos `encode` y `decode`
 import cipher from '../src/cipher';
-
 describe('cipher', () => {
-
   test('should be an object', () => {
     expect(typeof cipher).toBe('object');
   });
-
   describe('cipher.encode', () => {
-
     test('cifrarSms', () => {
       expect(typeof cipher.encode).toBe('function');
     });
     test('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
       expect(typeof cipher.encode).toBe('function');
-    });
-
-    
+    });   
   });
-
   describe('cipher.decode', () => {
-
     test('descifrar', () => {
       expect(typeof cipher.decode).toBe('function');
     });
-
     test('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
       expect(typeof cipher.decode).toBe('function');
     });
-
   });
-
 });
