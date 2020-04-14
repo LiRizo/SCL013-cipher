c631f72e0de9d9578d8878a359bdf07a51ae27d4
“LiRizo” <lirizoc@gmail.com>
2020-04-13 22:50:27 -0400

    cambios en src

--- src/cipher.js
+++ src/cipher.js
@@ -6,10 +6,8 @@ const cipher = {
       let resul = "";
       let x;
       let inCode = "";
-      let inCode1 = "";
       
       for (x = 0; x < texto.length; x++) {
-        
         if (texto.charCodeAt(x)>=65 && texto.charCodeAt(x)<=90){
           inCode= (((texto.charCodeAt(x)-65)+offset)%26)+65;
         }else if (texto.charCodeAt(x)>= 97 && texto.charCodeAt(x)<= 122){
@@ -17,8 +15,8 @@ const cipher = {
         resul += inCode;
       }else if (texto.charCodeAt(x) === 164){
         texto.charCodeAt(x) = ((texto.charCodeAt(x)- 164 + offset)% 1 + 164);
-          inCode1 = String.fromCharCode(texto.charCodeAt(x));
-          resul += inCode1;
+          inCode = String.fromCharCode(texto.charCodeAt(x));
+          resul += inCode;
       }else if(texto.charCodeAt(x)===32){
         inCode= 32;
       }resul += String.fromCharCode(inCode);
@@ -27,28 +25,28 @@ const cipher = {
     },
     //Descifrar
     
-    decode: (offset1,texto) => {
+    decode: (offset1,texto1) => {
     
-let resul = "";
-let x;
-let inCode = "";
-
-
-for (x = 0; x < texto.length; x++) {
-	
-	if (texto.charCodeAt(x)>=65 && texto.charCodeAt(x)<=90){
-		inCode= (((texto.charCodeAt(x)-65)-offset1)%26)+65;
-	}else if (texto.charCodeAt(x)>= 97 && texto.charCodeAt(x)<= 122){
-	inCode = String.fromCharCode((texto.charCodeAt(x)- 97 - offset1)% 26 + 97);
-	resul += inCode;	
-}else if(texto.charCodeAt(x)===164){
-	inCode= 164;
-}else if(texto.charCodeAt(x)===32){
-	inCode= 32;
-}
-	resul += String.fromCharCode(inCode);
+      let resul1 = "";
+      let x;
+      let inCode1 = "";
+      
+      
+      for (x = 0; x < texto1.length; x++) {
+        
+        if (texto1.charCodeAt(x)>=65 && texto1.charCodeAt(x)<=90){
+          inCode1= (((texto1.charCodeAt(x)-65)-offset1)%26)+65;
+        }else if (texto1.charCodeAt(x)>= 97 && texto1.charCodeAt(x)<= 122){
+        inCode1 = String.fromCharCode((texto1.charCodeAt(x)- 97 - offset1)% 26 + 97);
+        resul1 += inCode1;	
+      }else if(texto1.charCodeAt(x)===164){
+        inCode1= 164;
+      }else if(texto1.charCodeAt(x)===32){
+        inCode1= 32;
+      }
+        resul1 += String.fromCharCode(inCode1);
        }
-       return resul;
+       return resul1;
      }   
 };
 
--- src/index.js
+++ src/index.js
@@ -1,63 +1,39 @@
 import cipher from './cipher.js';
-alert("HolaMundo");
+
 document.getElementById("escogerCifrar").addEventListener("click",escogerCifrar);
-
 function escogerCifrar() {
-	let siguiente = document.getElementById("cifrar1");
-    siguiente.style.display = "block";
-
+	let siguienteC = document.getElementById("cifrar1");
+    siguienteC.style.display = "block";
     let bienvenida = document.getElementById("bienvenida");
     bienvenida.style.display = "none";	
 }
+
 document.getElementById("escogerDescifrar").addEventListener("click",escogerDescifrar);
 function escogerDescifrar() {
-	let siguiente = document.getElementById("descifrar1");
-    siguiente.style.display = "block";
+	let siguienteD = document.getElementById("descifrar1");
+    siguienteD.style.display = "block";
 
-    let bienvenida = document.getElementById("bienvenida");
-    bienvenida.style.display = "none";	
+    let bienvenida1 = document.getElementById("bienvenida");
+    bienvenida1.style.display = "none";	
 }
 
 //Parte para cifrar mensaje !!
-
 document.getElementById("cifrarSms").addEventListener("click",cifrarSms);
 
 function cifrarSms(){
 	let cifrarSmsActual = document.getElementById("cifrar1");
 	cifrarSmsActual.style.display = "none";
-// Esta parte es de los textarea
-cipher.encode(offset,texto);
-
-const offset = parseInt(document.getElementById("offset").value);//->Esto es para el Offset
-let texto = document.getElementById("textoCifrar1").value;
-/*let resul = "";
-let x;
-let inCode = "";
-let inCode1 = "";
-
-for (x = 0; x < texto.length; x++) {
-	
-	if (texto.charCodeAt(x)>=65 && texto.charCodeAt(x)<=90){
-		inCode= (((texto.charCodeAt(x)-65)+offset)%26)+65;
-	}else if (texto.charCodeAt(x)>= 97 && texto.charCodeAt(x)<= 122){
-	inCode = String.fromCharCode((texto.charCodeAt(x)- 97 + offset)% 26 + 97);
-	resul += inCode;
-}else if (texto.charCodeAt(x) === 164){
-	texto.charCodeAt(x) = ((texto.charCodeAt(x)- 164 + offset)% 1 + 164);
-		inCode1 = String.fromCharCode(texto.charCodeAt(x));
-		resul += inCode1;
-}else if(texto.charCodeAt(x)===32){
-	inCode= 32;
-}
-	resul += String.fromCharCode(inCode);*/
-document.getElementById("resultado").innerHTML = resul;
-}
-    // termina textarea 
 	let resultadoDelSms = document.getElementById("cifrar2");
 	resultadoDelSms.style.display = "block";
-  //}
+	// Esta parte es de los textarea
+const offset = parseInt(document.getElementById("offset").value);//->Esto es para el Offset
+let texto = document.getElementById("textoCifrar1").value;
+document.getElementById("resultado").innerHTML = cipher.encode (offset,texto);
+ }
+ 
   document.getElementById("clean").addEventListener("click",limpiar);
-  function limpiar() {
+  function limpiar(){   
+
 	document.getElementById("textoCifrar1").value = "";
 	document.getElementById("offset").value = ""; 
 	document.getElementById("textoDescifrar").value = "";
@@ -87,37 +63,16 @@ document.getElementById("descifrar").addEventListener("click",descifrar);
 function descifrar(){                       
 	let recibirCodActual = document.getElementById("descifrar1");
 	recibirCodActual.style.display = "none";
-
-	cipher.decode(offset1,texto);
-const offset1 = parseInt(document.getElementById("offset1").value);//->Esto es para el Offset
-let texto = document.getElementById("textoDescifrar").value;
-/*let resul = "";
-let x;
-let inCode = "";
-
-
-for (x = 0; x < texto.length; x++) {
-	
-	if (texto.charCodeAt(x)>=65 && texto.charCodeAt(x)<=90){
-		inCode= (((texto.charCodeAt(x)-65)-offset1)%26)+65;
-	}else if (texto.charCodeAt(x)>= 97 && texto.charCodeAt(x)<= 122){
-	inCode = String.fromCharCode((texto.charCodeAt(x)- 97 - offset1)% 26 + 97);
-	resul += inCode;	
-}else if(texto.charCodeAt(x)===164){
-	inCode= 164;
-}else if(texto.charCodeAt(x)===32){
-	inCode= 32;
-}
-	resul += String.fromCharCode(inCode);*/
-document.getElementById("resultado2").innerHTML = resul;
-//}
-  
 	let inicioSiguiente = document.getElementById("descifrar2");
 	inicioSiguiente.style.display = "block";
+	// Esta parte es de los textarea
+	const offset1 = parseInt(document.getElementById("offset1").value);//->Esto es para el Offset
+let texto1 = document.getElementById("textoDescifrar").value;
+document.getElementById("resultado2").innerHTML = cipher.decode (offset1,texto1);
   }
+
   document.getElementById("si2").addEventListener("click",cifrarSmsV1);
   function cifrarSmsV1(){
-	
 	let descifradoAtual = document.getElementById("descifrar2");
 	descifradoAtual.style.display = "none";
   
